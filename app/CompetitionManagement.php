8f190f1cda9b0eaed4860d43bb3a0cf2b1680274
andikaandra <andikaandra03@gmail.com>
2019-04-08 02:05:11 +0700

    add model

---
+++ app/CompetitionManagement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CompetitionManagement extends Model
+{
+    protected $table = 'competition_management';
+    protected $guarded = [];
+}

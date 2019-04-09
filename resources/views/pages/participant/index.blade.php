c92de8eb33d24a3c049ca25ef1e75bb3068f9c62
adis azhar <adisazhar123@gmail.com>
2019-04-09 16:06:44 +0700

    email verif

--- resources/views/pages/participant/index.blade.php
+++ resources/views/pages/participant/index.blade.php
@@ -6,24 +6,26 @@
 
 @endsection
 @section('content')
-            <div class="card card-content">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-sm-5">
-                    <h4 class="card-title mb-0">AMSW <?php echo date("Y"); ?></h4>
-                    <div class="small text-muted">Dashboard</div>
-                  </div>
-                  <div class="col-sm-7 d-none d-md-block">
+    <div class="card card-content">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-sm-5">
+            <h4 class="card-title mb-0">AMSW <?php echo date("Y"); ?></h4>
+            <div class="small text-muted">Dashboard</div>
+          </div>
+          <div class="col-sm-7 d-none d-md-block">            
+          </div>          
+        </div>
+        <hr>
+        <div class="chart-wrapper mt-3" style="min-height:300px;">
+          {{-- Not verified yet --}}
+          <div class="email-unverified">
+            <p>Hello <strong>{{Auth::user()->name}}</strong>. <br> Welcome to your AMSW Web Dashboard. Please verify your account by checking the email we sent you. Open your mailbox using the email you registered with.</p>
+          </div>
+        </div>
+      </div>
+      <div class="card-footer">
 
-                  </div>
-                </div>
-                <hr>
-                <div class="chart-wrapper mt-3" style="min-height:300px;">
-                  
-                </div>
-              </div>
-              <div class="card-footer">
-
-              </div>
-            </div>
+      </div>
+    </div>
 @endsection
--- resources/views/pages/participant/registration.blade.php
+++ resources/views/pages/participant/registration.blade.php
@@ -6,42 +6,42 @@
 
 @endsection
 @section('content')
-            <div class="card card-content">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-sm-5">
-                    <h4 class="card-title mb-0">Registration</h4>
-                    <div class="small text-muted">AMSW <?php echo date("Y"); ?></div>
-                  </div>
-                  <div class="col-sm-7 d-none d-md-block">
+    <div class="card card-content">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-sm-5">
+            <h4 class="card-title mb-0">Registration</h4>
+            <div class="small text-muted">AMSW <?php echo date("Y"); ?></div>
+          </div>
+          <div class="col-sm-7 d-none d-md-block">
 
-                  </div>
-                </div>
-                <hr>
-                @if (\Session::has('success'))
-                    <div class="alert alert-success">
-                      {!! \Session::get('success') !!}
-                    </div>
-                @endif
-                @if (\Session::has('error'))
-                    <div class="alert alert-danger">
-                      {!! \Session::get('error') !!}
-                    </div>
-                @endif
-                <div class="chart-wrapper mt-3" style="min-height:300px;">
-                  @if(Auth::user()->status==1)
-                    @include('includes.participant.form.choose-cabang')
-                  @elseif(Auth::user()->status==2)
-                    form upload personal data dan bukti pembayaran
-                  @elseif(Auth::user()->status==2)
-                    notifikasi menunggu pemberitahuan admin
-                  @endif
-                </div>
-              </div>
-              <div class="card-footer">
-
-              </div>
+          </div>
+        </div>
+        <hr>
+        @if (\Session::has('success'))
+            <div class="alert alert-success">
+              {!! \Session::get('success') !!}
             </div>
+        @endif
+        @if (\Session::has('error'))
+            <div class="alert alert-danger">
+              {!! \Session::get('error') !!}
+            </div>
+        @endif
+        <div class="chart-wrapper mt-3" style="min-height:300px;">
+          @if(Auth::user()->status==1)
+            @include('includes.participant.form.choose-cabang')
+          @elseif(Auth::user()->status==2)
+            form upload personal data dan bukti pembayaran
+          @elseif(Auth::user()->status==2)
+            notifikasi menunggu pemberitahuan admin
+          @endif
+        </div>
+      </div>
+      <div class="card-footer">
+
+      </div>
+    </div>
 @endsection
 
 @section('script')

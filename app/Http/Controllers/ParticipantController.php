f8962420e835684a1b88509c8a3e8e552b0de1a9
andikaandra <andikaandra03@gmail.com>
2019-04-09 14:05:52 +0700

    update

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -15,15 +15,4 @@ class ParticipantController extends Controller
     public function registrationPage() {
         return view('pages.participant.registration');
     }
-
-    public function chooseCabang(Request $request) {
-		try {
-	    	$update = User::where('id', $request->user)->update(['status' => 2, 'competition' => $request->competition]);
-		} catch (\Exception $e) {
-	        $eMessage = 'Choose Competition - User: ' . $request->user . ', error: ' . $e->getMessage();
-	        Log::emergency($eMessage);
-	    	return redirect()->back()->with('error', 'Whoops, something error!'); 
-	    }
-        return redirect()->back()->with('success', 'Successfully choose competition branch'); 
-    }
 }

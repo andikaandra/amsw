6b1c10b24a0c302acecd08283f30afdac1f28bae
andikaandra <andikaandra03@gmail.com>
2019-05-20 00:16:10 +0700

    disable guidebook donwload

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -11,22 +11,27 @@ class HomepageController extends Controller
     }
 
     public function researchPaperGuidebook() {
+        return "Guidebook not found";
         return response()->download(storage_path('app') .'/public/amsw-files/guidebook/research-paper-guidebook.zip');
     }
 
 	public function literatureReviewGuidebook() {
+        return "Guidebook not found";
         return response()->download(storage_path('app') .'/public/amsw-files/guidebook/literature-review-guidebook.zip');
     }
 
 	public function scientificEssayGuidebook() {
+        return "Guidebook not found";
         return response()->download(storage_path('app') .'/public/amsw-files/guidebook/essay-guidebook.zip');
     }
 
 	public function publicPosterGuidebook() {
+        return "Guidebook not found";
         return response()->download(storage_path('app') .'/public/amsw-files/guidebook/public-poster-guidebook.zip');
     }
 
 	public function educationalVideoGuidebook() {
+        return "Guidebook not found";
         return response()->download(storage_path('app') .'/public/amsw-files/guidebook/video-champaign-guidebook.zip');
     }
 
